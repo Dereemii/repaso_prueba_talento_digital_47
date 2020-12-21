b4ddafab3ed9219c87915fb55d14568283ecf6aa
Alejandro Bonilla <alebonilla01@gmail.com>
2020-12-21 20:29:39 -0300

    Vista login listo

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Course from '@/views/Course.vue'
+import Login from '@/views/Login.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -16,6 +17,11 @@ const routes = [
     component: Course,
     props:true,
 
+  },
+  {
+    path: '/login',
+    name: "Login",
+    component: Login,
   },
   {
     path: '/about',
---
+++ src/views/Login.vue
@@ -0,0 +1,69 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col md="6">
+        <h1>Login</h1>
+        <v-card>
+          <v-form ref="form" v-model="valid" lazy-validation>
+        
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="Correo"
+            required
+          ></v-text-field>
+
+           <v-text-field
+            v-model="pass"
+            :rules="[(v) => !!v || 'Contraseña requerida']"
+            label="Contraseña"
+            type="password"
+            required
+          ></v-text-field>
+
+      
+
+          <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="validate"
+          >
+            Ingresar
+          </v-btn>
+
+          <v-btn color="error" class="mr-4" @click="reset">
+            Limpiar
+          </v-btn>
+
+        </v-form>
+        </v-card>
+        
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "LoginComponent",
+  data: function(){
+    return{
+      valid: true,
+      email:"",
+      pass: "",
+      emailRules: [
+        v => !!v || 'Correo requerido',
+        v => /.+@.+\..+/.test(v) || 'Correo no valido',
+      ],
+    }
+  },
+  methods:{
+    validate () {
+        this.$refs.form.validate()
+      },
+  }
+};
+</script>
+
+<style></style>
